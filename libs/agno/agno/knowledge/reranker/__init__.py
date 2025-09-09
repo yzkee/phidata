b8f61c757a38198741208935c4d035c4ef131e93
ysolanky <yash@phidata.com>
2025-09-09 11:47:31 -0400

    fix: reranker imports

--- libs/agno/agno/knowledge/reranker/__init__.py
+++ libs/agno/agno/knowledge/reranker/__init__.py
@@ -1,9 +1,3 @@
-from agno.knowledge.reranker.cohere import CohereReranker
-from agno.knowledge.reranker.infinity import InfinityReranker
-from agno.knowledge.reranker.sentence_transformer import SentenceTransformerReranker
+from agno.knowledge.reranker.base import Reranker
 
-__all__ = [
-    "CohereReranker",
-    "InfinityReranker",
-    "SentenceTransformerReranker",
-]
+__all__ = ["Reranker"]
