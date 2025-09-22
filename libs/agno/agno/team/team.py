e31cd2ce75a7f58460f81d5f0fe907370ba2e783
Dirk Brand <51947788+dirkbrnd@users.noreply.github.com>
2025-09-22 11:16:33 +0200

    Revert "fix: favor session db state for subsequent runs" (#4732)
    Reverts agno-agi/agno#4704

--- libs/agno/agno/team/team.py
+++ libs/agno/agno/team/team.py
@@ -6086,7 +6086,10 @@ class Team:
                 and isinstance(session_state_from_db, dict)
                 and len(session_state_from_db) > 0
             ):
-                merge_dictionaries(session_state, session_state_from_db)
+                # This updates session_state_from_db
+                # If there are conflicting keys, values from provided session_state will take precedence
+                merge_dictionaries(session_state_from_db, session_state)
+                session_state = session_state_from_db
 
         # Update the session_state in the session
         if session.session_data is not None:
