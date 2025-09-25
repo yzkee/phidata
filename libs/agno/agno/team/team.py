e1d091eb97589b621b7d927e28c14557bfd4253c
ysolanky <yash@phidata.com>
2025-09-24 23:20:03 -0400

    fix: get_members_system_message_content fn

--- libs/agno/agno/team/team.py
+++ libs/agno/agno/team/team.py
@@ -4270,7 +4270,7 @@ class Team:
                     system_message_content += f"{indent * ' '}   - Name: {member.name}\n"
                 if member.role is not None:
                     system_message_content += f"{indent * ' '}   - Role: {member.role}\n"
-                if member.tools is not None and self.add_member_tools_to_context:
+                if member.tools != [] and self.add_member_tools_to_context:
                     system_message_content += f"{indent * ' '}   - Member tools:\n"
                     for _tool in member.tools:
                         if isinstance(_tool, Toolkit):
