cfa6a790aedf6b554a467f12007ce14344aff678
kausmeows <shuklakaustubh84@gmail.com>
2026-01-19 17:58:19 +0530

    chore: default agentos db

---
+++ cookbook/06_agent_os/dbs/agentos_default_db.py
@@ -0,0 +1,56 @@
+"""
+AgentOS Demo
+
+Set the OS_SECURITY_KEY environment variable to your OS security key to enable authentication.
+
+Prerequisites:
+pip install -U fastapi uvicorn sqlalchemy pgvector psycopg openai ddgs yfinance
+"""
+
+from agno.agent import Agent
+from agno.db.postgres import PostgresDb
+from agno.knowledge.knowledge import Knowledge
+from agno.models.openai import OpenAIChat
+from agno.os import AgentOS
+from agno.team import Team
+from agno.tools.mcp import MCPTools
+from agno.tools.websearch import WebSearchTools
+from agno.vectordb.pgvector import PgVector
+
+# Database connection
+db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
+
+# Create Postgres-backed memory store
+db = PostgresDb(db_url=db_url)
+
+# Create Postgres-backed vector store
+vector_db = PgVector(
+    db_url=db_url,
+    table_name="agno_docs",
+)
+knowledge = Knowledge(
+    name="Agno Docs",
+    contents_db=db,
+    vector_db=vector_db,
+)
+
+# Create your agents
+agno_agent = Agent(
+    name="Agno Agent",
+    model=OpenAIChat(id="gpt-4.1"),
+    tools=[MCPTools(transport="streamable-http", url="https://docs.agno.com/mcp")],
+    knowledge=knowledge,
+    markdown=True,
+)
+
+# Create the AgentOS
+agent_os = AgentOS(
+    id="agentos-demo",
+    agents=[agno_agent],
+    db=db, # This is the default database for AgentOS, the agno_agent will use this
+)
+app = agent_os.get_app()
+
+
+if __name__ == "__main__":
+    agent_os.serve(app="agentos_default_db:app", port=7777, reload=True)
