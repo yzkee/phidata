51865203075105cbe1dab2f5282545d72bd8dec4
guguoyi <2390049085@qq.com>
2026-02-03 17:32:18 +0800

    cookbook: refine typos (#6212)
    ## Summary
    
    Refine some typos when use LightRag
    
    ## Type of change
    
    - [ ] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Improvement
    - [ ] Model update
    - [x] Other:
    
    ---
    
    ## Checklist
    
    ---
    
    ## Additional Notes
    
    Co-authored-by: Milo Gu <milo.gu@maiscrm.com>
    Co-authored-by: Kaustubh <shuklakaustubh84@gmail.com>

--- cookbook/02_agents/agentic_search/lightrag/agentic_rag_with_lightrag.py
+++ cookbook/02_agents/agentic_search/lightrag/agentic_rag_with_lightrag.py
@@ -11,8 +11,8 @@ vector_db = LightRag(
 )
 
 knowledge = Knowledge(
-    name="My Pinecone Knowledge Base",
-    description="This is a knowledge base that uses a Pinecone Vector DB",
+    name="My LightRag Knowledge Base",
+    description="This is a knowledge base that uses a LightRag Vector DB",
     vector_db=vector_db,
 )
 
